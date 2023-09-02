9817cc6d34f6125c2843c870ee2b8ffa834e8dae
fatemeabedi <abedifateme1378@gmail.com>
2023-09-02 12:17:23 +0330

    add nwe questios

---
+++ algorithm/solve-me-first/answer.js
@@ -0,0 +1,8 @@
+function solveMeFirst(a, b) {
+   
+    let result = a + b;
+  
+    return result;
+  }
+  
+  console.log(solveMeFirst(a, b));  // Output: 5
---
+++ algorithm/very-big-sum/answer.js
@@ -0,0 +1,5 @@
+function veryBigSum(ar){
+    return ar.reduce((acc, curr) => acc + curr, 0);
+        };
+   
+   console.log(bigSum([1000000001, 1000000002, 1000000003, 1000000004 ,1000000005]));  result:5000000015
