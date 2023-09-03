54e0893b0b25d5ade6f8866e2eb955107bcaa6db
fateme abedi <94634719+fateme-abedi@users.noreply.github.com>
2023-09-03 17:26:12 +0330

    Update answer.js

--- datastructure/array-left-rotation/answer.js
+++ datastructure/array-left-rotation/answer.js
@@ -1,7 +1,6 @@
 function rotateLeft(d, arr) {
     const n = arr.length;
     const rotatedArr = [];
-
     for (let i = 0; i < n; i++) {
         const newIndex = (i + n - d) % n;
         rotatedArr[newIndex] = arr[i];
@@ -13,28 +12,15 @@ function rotateLeft(d, arr) {
 //OR
 
 //      function rotateLeft(d,arr){
-//     let rotatedArr=[];
-//     for(i=0; i<arr.length; i++){
-//         let newIndex=i-d;
-//         if(newIndex<0){
-//             newIndex=arr.length -d +i;
+//       let rotatedArr = [];
+//       for(i=0; i < arr.length; i++){
+//         let newIndex = i-d;
+//         if(newIndex < 0){
+//             newIndex = arr.length -d +i;
 //         }
-
-//         rotatedArr[newIndex]=arr[i]
-
+//         rotatedArr[newIndex] = arr[i];
 //     }
-//     return rotatedArr;
-// }(d,arr){
-//     let rotatedArr=[];
-//     for(i=0; i<arr.length; i++){
-//         let newIndex=i-d;
-//         if(newIndex<0){
-//             newIndex=arr.length -d +i;
-//         }
 
-//         rotatedArr[newIndex]=arr[i]
-
-//     }
 //     return rotatedArr;
 // }
  console.log(rotateLeft(2,[1,2,3,4,5]));  result:[3,4,5,1,2]
