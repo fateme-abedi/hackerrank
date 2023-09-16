24e24500951ebf877d040df878dc99bacee4779e
fatemeabedi <abedifateme1378@gmail.com>
2023-09-16 16:34:52 +0330

    mini-max sum add to algo

---
+++ algorithm/mini-max-sum/answer.js
@@ -0,0 +1,6 @@
+function miniMaxSum(arr) {
+  let sum = arr.reduce((acc, curr) => acc + curr, 0);
+  let minSum = sum - Math.max(...arr);
+  let maxSum = sum - Math.min(...arr);
+  console.log(minSum + " " + maxSum);
+}
