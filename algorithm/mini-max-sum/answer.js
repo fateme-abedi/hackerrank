a45b957332808daa03ff770f2b6b6ce7a1292b2e
fatemeabedi <abedifateme1378@gmail.com>
2023-09-20 16:08:54 +0330

    add to algo

--- algorithm/mini-max-sum/answer.js
+++ algorithm/mini-max-sum/answer.js
@@ -2,5 +2,5 @@ function miniMaxSum(arr) {
   let sum = arr.reduce((acc, curr) => acc + curr, 0);
   let minSum = sum - Math.max(...arr);
   let maxSum = sum - Math.min(...arr);
-  console.log(minSum + " " + maxSum);
+  console.log(minSum, maxSum);
 }
